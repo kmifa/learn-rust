89adcdc80409a12f0b5031a0604cc4ba23162c53
shinji asako <13231699+kmifa@users.noreply.github.com>
2023-09-12 09:51:36 +0900

    テスト作成

--- easy-quiz/src/main.rs
+++ easy-quiz/src/main.rs
@@ -1,16 +1,17 @@
 use std::io;
 
+// 定数の場合は、型を明示する必要がある
+const QUIZ: [(&str, &str); 5] = [
+    ("apple", "りんご"),
+    ("banana", "ばなな"),
+    ("orange", "おれんじ"),
+    ("peach", "もも"),
+    ("grape", "ぶどう"),
+];
+
 fn main() {
     println!("英単語をランダムに表示します。対応する日本語をひらがなで入力してね！");
     println!("問題は5問あるよ！");
-
-    let quiz = [
-        ("apple", "りんご"),
-        ("banana", "ばなな"),
-        ("orange", "おれんじ"),
-        ("peach", "もも"),
-        ("grape", "ぶどう"),
-    ];
 
     // 答え合わせ用の変数
     let mut count = 0;
@@ -18,29 +19,28 @@ fn main() {
     // ループ用の変数
     let mut i = 0;
     loop {
-        println!("{}問目! {} ", i + 1, quiz[i].0);
+        println!("{}問目! {} ", i + 1, QUIZ[i].0);
         let mut guess = String::new();
 
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        // 不正入力検知
-        // let guess: u32 = match guess.trim().parse() {
-        //     Ok(num) => num,
-        //     Err(_) => continue,
-        // };
-
-        // read_line メソッドを使用すると、入力の終わりに改行文字（\n）が含まれる点にあります
+        // read_line メソッドを使用すると、入力の終わりに改行文字（\n）が含まれるのtirm()で削除
         let d = guess.trim();
 
         // 答え合わせ
-        if d == quiz[i].1 {
-            println!("正解！");
-            count += 1;
-        } else {
-            println!("不正解！");
-            println!("{}", quiz[i].1);
+        let check = answer_check(d, QUIZ[i].1);
+
+        match check {
+            true => {
+                println!("正解！");
+                count += 1;
+            }
+            false => {
+                println!("不正解！");
+                println!("{}", QUIZ[i].1);
+            }
         }
 
         // 5問終わったら終了
@@ -53,4 +53,27 @@ fn main() {
     }
 
     // 入力文字を保持するための変数
+}
+
+fn answer_check(input_word: &str, quiz_answer: &str) -> bool {
+    if input_word == quiz_answer {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn _りんごと入力するとtrueを返す() {
+        assert_eq!(true, answer_check("りんご", QUIZ[0].1));
+    }
+
+    #[test]
+    fn _改行コードを入力するとfalseを返す() {
+        assert_eq!(false, answer_check("りんご\n", QUIZ[0].1));
+    }
 }
