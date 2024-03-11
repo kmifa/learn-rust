f0faa82e0d073aa490a8edbbdadad5db9bef095d
shinji asako <13231699+kmifa@users.noreply.github.com>
2024-03-11 22:29:30 +0900

    言語処理100本ノック 01

--- nlp100/src/main.rs
+++ nlp100/src/main.rs
@@ -1,6 +1,17 @@
 fn main() {
+    nlp_01();
+}
+
+fn nlp_00() {
     let str = "stressed";
     let re = str.chars().rev().collect::<String>();
     println!("{}", re);
     assert_eq!(re, "desserts");
+}
+
+fn nlp_01() {
+    let str = "パタトクカシーー";
+    let re = str.chars().step_by(2).collect::<String>();
+    println!("{}", re);
+    assert_eq!(re, "パトカー");
 }
