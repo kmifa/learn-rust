0d98a29c953285ed0d5b6527da731f2e2f848907
shinji asako <13231699+kmifa@users.noreply.github.com>
2024-03-12 11:16:07 +0900

    言語処理100本ノック 02

--- nlp100/src/main.rs
+++ nlp100/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    nlp_01();
+    nlp_02();
 }
 
 fn nlp_00() {
@@ -14,4 +14,17 @@ fn nlp_01() {
     let re = str.chars().step_by(2).collect::<String>();
     println!("{}", re);
     assert_eq!(re, "パトカー");
+}
+
+fn nlp_02() {
+    let str1 = "パトカー";
+    let str2 = "タクシー";
+    let mut re = String::new(); // Initialize the `str` variable
+    for (c1, c2) in str1.chars().zip(str2.chars()) {
+        re.push(c1);
+        re.push(c2);
+    }
+
+    println!("{}", re);
+    assert_eq!(re, "パタトクカシーー");
 }
