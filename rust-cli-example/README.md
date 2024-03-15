afc5734f68c5ae96706e0cad2fef1a970bb82bf7
shinji asako <13231699+kmifa@users.noreply.github.com>
2024-03-15 23:39:03 +0900

    TUIアプリ途中まで

---
+++ rust-cli-example/README.md
@@ -0,0 +1,1 @@
+https://blog.logrocket.com/rust-and-tui-building-a-command-line-interface-in-rust/
---
+++ rust-cli-example/src/main.rs
@@ -0,0 +1,93 @@
+use std::{
+    io,
+    sync::mpsc,
+    thread,
+    time::{Duration, Instant},
+};
+
+use chrono::{DateTime, Utc};
+use crossterm::{
+    event::{self, Event as CEvent, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+use tui::{backend::CrosstermBackend, Terminal};
+
+const DB_PATH: &str = "./data/db.json";
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Pet {
+    id: usize,
+    name: String,
+    category: String,
+    age: usize,
+    created_at: DateTime<Utc>,
+}
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("error reading the DB file: {0}")]
+    ReadDBError(#[from] io::Error),
+    #[error("error parsing the DB file: {0}")]
+    ParseDBError(#[from] serde_json::Error),
+}
+
+enum Event<I> {
+    Input(I),
+    Tick,
+}
+
+#[derive(Copy, Clone, Debug)]
+enum MenuItem {
+    Home,
+    Pets,
+}
+
+impl From<MenuItem> for usize {
+    fn from(input: MenuItem) -> usize {
+        match input {
+            MenuItem::Home => 0,
+            MenuItem::Pets => 1,
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // terminalのrow modeを有効にする
+    enable_raw_mode().expect_err("can run in raw mode");
+
+    // チャネルを作成
+    let (tx, rx) = mpsc::channel();
+
+    // 入力イベントを受け付ける時間(200ミリ秒に設定)
+    let tick_rate = Duration::from_millis(200);
+
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            if event::poll(timeout).expect("poll works") {
+                if let CEvent::Key(key) = event::read().expect("can read events") {
+                    tx.send(Event::Input(key)).expect("can send events");
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate {
+                if let Ok(_) = tx.send(Event::Tick) {
+                    last_tick = Instant::now();
+                }
+            }
+        }
+    });
+
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+
+    return Ok(());
+}
