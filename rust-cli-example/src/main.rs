973eea0c58707688ba800b293a7335cbc0b8d7ff
shinji asako <13231699+kmifa@users.noreply.github.com>
2024-03-20 23:37:40 +0900

    途中まで

--- rust-cli-example/src/main.rs
+++ rust-cli-example/src/main.rs
@@ -15,8 +15,9 @@ use thiserror::Error;
 use tui::{
     backend::CrosstermBackend,
     layout::{Alignment, Constraint, Direction, Layout},
-    style::{Color, Style},
-    widgets::{Block, BorderType, Borders, Paragraph},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{Block, BorderType, Borders, Paragraph, Tabs},
     Terminal,
 };
 
@@ -125,7 +126,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         .title("Copyright")
                         .border_type(BorderType::Plain),
                 );
-            rect.render_widget(copyright, chunks[2])
+
+            let menu = menu_titles
+                .iter()
+                .map(|t| {
+                    let (first, rest) = t.split_at(1);
+                    Spans::from(vec![
+                        Span::styled(
+                            first,
+                            Style::default()
+                                .fg(Color::Yellow)
+                                .add_modifier(Modifier::UNDERLINED),
+                        ),
+                        Span::styled(rest, Style::default().fg(Color::White)),
+                    ])
+                })
+                .collect();
+
+            let tabs = Tabs::new(menu)
+                .select(active_menu_item.into())
+                .block(Block::default().title("Menu").borders(Borders::ALL))
+                .style(Style::default().fg(Color::White))
+                .highlight_style(Style::default().fg(Color::Yellow))
+                .divider(Span::raw("|"));
+
+            rect.render_widget(copyright, chunks[2]);
         });
     }
 
